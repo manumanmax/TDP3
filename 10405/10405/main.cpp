c9ff1e388710f58f122fc26a956c2df577506a3e
manumanmax <manua380@hotmail.fr>
2015-11-26 22:42:58 +0100

    input test

---
+++ 10405/10405/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    string input;
+    cin >> input;
+    cout << input << endl;
+    cout << "CR check" << endl;
+    return 0;
+}
