932f098f905f9811ffc36ae96495cc4a6976a61f
manumanmax <manua380@hotmail.fr>
2015-11-27 19:04:20 +0100

    project 103 string parsing and boxes building

--- 10405/10405/main.cpp
+++ 10405/10405/main.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 using namespace std;
 
+string getOneSubstring(string localString, int idx1, int idx2, const string s1, const string s2);
+
+/*
 int counter(int size, int index1, int index2, const string input1, const string input2){
     int elseLocalMin = 0;
     int localMaximum = 0;
@@ -28,29 +31,36 @@ int counter(int size, int index1, int index2, const string input1, const string 
         return localMaximum;
     else
         return size;
-}
+}*/
 
 string getSubstrings(vector<string>& vStrings, string substring, int idx1, int idx2, const string s1, const string s2){
 
+    int localIdx2 = idx2;
+
     for(; idx1 < s1.length(); idx1++){
-        for(; idx2 < s2.length(); idx2++){
+        for(idx2 = localIdx2; idx2 < s2.length(); idx2++){
+            //printf("%d -> ",idx2);
             if(s1[idx1] == s2[idx2]){
+                localIdx2 = idx2+1;
                 substring.push_back(s1[idx1]);
-            }else{
-                    string localString = "";
-                    localString.push_back(s2[idx1]);
-                    vStrings.push_back(getSubstrings(vStrings, localString, idx2, idx1+1, s2,s1));
-
-                    break;
-
+                break;
             }
+
+                //substring.push_back(s2[idx1]);
+                //string localString = substring;
+
+
         }
+         vStrings.push_back(getOneSubstring("", idx1, 0, s1, s2));
+
+        //cout << endl;
     }
     return substring;
+
 }
 
-string getOneSubstring(int idx1, int idx2, const string s1, const string s2){
-    string substring;
+string getOneSubstring(string substring, int idx1, int idx2, const string s1, const string s2){
+
     int localIdx2 = idx2;
     for(; idx1 < s1.length(); idx1++){
         for(idx2 = localIdx2; idx2 < s2.length(); idx2++){
@@ -61,32 +71,33 @@ string getOneSubstring(int idx1, int idx2, const string s1, const string s2){
                 break;
             }
         }
-        cout << endl;
+        //cout << endl;
     }
     return substring;
 }
 
 
+
+
 int main()
 {
-    string input1 = "abcdgh";
-    string input2 = "aedfhr";
+    string input1 = "bcacbcabbaccbab";
+    string input2 = "bccabccbbabacbc";
 
     //cin >> input1;
     //cin >> input2;
-    /*vector<string> vS;
+    vector<string> vS;
     vS.push_back(getSubstrings(vS,"",0,0,input1,input2));
+    vS.push_back(getSubstrings(vS,"",0,0,input2,input1));
     int sizeMax = 0;
     for(string str: vS){
             cout << str << endl;
         if(str.length() > sizeMax)
             sizeMax = str.length();
-    }*/
+    }
+    cout << sizeMax << endl;
 
-    //cout << sizeMax << endl;
-
-    cout << getOneSubstring(0, 0, input1, input2);
-
+    cout << getOneSubstring("",0,0,input2,input1) << endl;
 
     return 0;
 }
